4602a0928ea35e2814d00c76e8562ff685864fde
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-10-08 22:46:37 -0300

    Refactor main.dart: Remove commented out code

--- lib/main.dart
+++ lib/main.dart
@@ -9,31 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Mini Projeto 01',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home:  Scaffold(
-//         appBar: AppBar(
-//           titleTextStyle: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w600),
-//           backgroundColor: Colors.blueGrey,
-//           title:  Text("Application", style: TextStyle(color: Colors.white),),
-//           actions: [
-//             IconButton(icon: Icon(Icons.more_vert, color: Colors.white,), onPressed: () {  },)
-//           ],
-//         ),
-//         body: const Center(
-//           child: CalcIMC(),
-//         ),
-//       )
-//     );
-//   }
-// }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
